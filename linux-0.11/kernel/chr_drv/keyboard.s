44382847c02fa991f7f7a0a6823b45fd4487530f
hitlzm <hitlzm@yeah.net>
2016-01-04 13:26:52 +0800

    oslab-3 release 完成了时间片的修改，报告

--- linux-0.11/kernel/chr_drv/keyboard.s
+++ linux-0.11/kernel/chr_drv/keyboard.s
@@ -1,5 +1,5 @@
 # 1 "keyboard.S"
-# 1 "/home/zhiming/oslab/oslab-3/linux-0.11/kernel/chr_drv//"
+# 1 "/home/zhiming/oslab/linux-0.11/kernel/chr_drv//"
 # 1 "<built-in>"
 # 1 "<command line>"
 # 1 "keyboard.S"
